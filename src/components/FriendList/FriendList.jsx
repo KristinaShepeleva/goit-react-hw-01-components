123d430fbc1ef1e3fd972ef58ae12ea7b3097e0e
KristinaShepeleva <shepelevakristina52@gmail.com>
2023-04-24 11:56:46 +0300

    fix-profile

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,26 +1,33 @@
 import PropTypes from 'prop-types';
 import css from './FriendList.module.css'
 
-export const FriendList = ({ friends }) => {
-
-    return (
-      <ul className={css.friend__list}>
-        
-        {friends.map(({ id, avatar, name, isOnline }) => {
-          
+const FriendListItem = ({ id, avatar, name, isOnline }) => {        
         return (
-        <li className={css.item} key={id}>
+        <li className={css.item}>
                 <span className={`${css.status} ${css[isOnline]}`}>
               {isOnline}
             </span>   
         <img className={css.avatar} src={avatar} alt={name} width="48"/>
           <p className={css.name}>{name}</p>              
         </li>)
-            })} 
+ }
+
+export const FriendList =({ friends }) => {
+    return (
+      <ul className={css.friend__list}>
+
+        {friends.map(({ id, avatar, name, isOnline }) => {
+          return (
+          <FriendListItem 
+            key={id}
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline} />
+          )
+       })} 
 </ul>
     )
 }
-
 
 FriendList.propTypes = {
     friends: PropTypes.arrayOf(PropTypes.shape({
@@ -30,3 +37,4 @@ FriendList.propTypes = {
     id: PropTypes.number.isRequired,
     })).isRequired,  
 }
+
